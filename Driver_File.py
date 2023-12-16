58d196481ccd242398947abe58e1a31f9b29b151
kikofers <dolelext@gmail.com>
2023-12-16 21:53:27 +0200

    importējam bibliotēkas

--- Driver_File.py
+++ Driver_File.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.layers import Flatten, Dense, Dropout, Convolution2D, MaxPooling2D
-from keras.utils import np_utils
+import keras
+from keras.layers import Flatten, Dense, Dropout, Convolution2D, MaxPooling2D
+from keras.utils import to_categorical
 
 import numpy as np
 import matplotlib.pyplot as plt
