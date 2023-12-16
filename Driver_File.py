daf11844b12d2863077604b2ccff020fc3b46ca2
kikofers <dolelext@gmail.com>
2023-12-16 21:39:17 +0200

    importējam bibliotēkas

---
+++ Driver_File.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Flatten, Dense, Dropout, Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+
+import numpy as np
+import matplotlib.pyplot as plt
+
