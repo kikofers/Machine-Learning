aa26e1abbe0ced335069c68e996134d3a651c8fc
kikofers <dolelext@gmail.com>
2023-12-17 19:06:02 +0200

    Uzstaisiju modeli. Atliek trenet

--- Driver_File.py
+++ Driver_File.py
@@ -1,7 +1,11 @@
+import sys
+from keras.utils import to_categorical 
 from keras.layers import Flatten, Dense, Convolution2D, MaxPooling2D
 from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
+from scipy import misc
 
 # Izveido modeli.
 def izveido_modeli():
@@ -16,9 +20,27 @@ def izveido_modeli():
     modelis.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
     return modelis
 
-modelis = izveido_modeli()
+def secinajums(vesture):
+    pyplot.subplot(211)
+    pyplot.title('Zaudējums')
+    pyplot.plot(vesture.history['loss'], color='blue', label='trenēšana')
+    pyplot.plot(vesture.history['val_loss'], color='orange', label='tests')
+    pyplot.subplot(212)
+    pyplot.title('Precizitāte')
+    pyplot.plot(vesture.history['accuracy'], color='blue', label='trenēšana')
+    pyplot.plot(vesture.history['val_accuracy'], color='orange', label='tests')
+    pyplot.tight_layout()
+    pyplot.show()
 
-datagen = ImageDataGenerator(rescale=1.0/255.0)
+def testejam():
+    modelis = izveido_modeli()
+    datagen = ImageDataGenerator(rescale=1.0/255.0)
+    # Iedala datus trenēšanas un testēšanas datu kopās.
+    trenins = datagen.flow_from_directory("training_set/", class_mode='binary', batch_size=64, target_size=(200, 200))
+    tests = datagen.flow_from_directory("test_set/", class_mode='binary', batch_size=64, target_size=(200, 200))
+    vesture = modelis.fit(trenins, steps_per_epoch=len(trenins), validation_data=tests, validation_steps=len(tests), epochs=20, verbose=0)
+    _, acc = modelis.evaluate_generator(tests, steps=len(tests), verbose=0)
+    print('> %.3f' % (acc * 100.0))
+    secinajums(vesture)
 
-# Iedala datus trenēšanas un testēšanas datu kopās.
-
+testejam()
