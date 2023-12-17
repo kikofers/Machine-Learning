5963c22adf1834d29f0ca2176f6d72bbfe008bd9
kikofers <dolelext@gmail.com>
2023-12-17 15:22:25 +0200

    Dati ir dabūti. Kodam ir piekļuve pie tiem.

--- Driver_File.py
+++ Driver_File.py
@@ -5,4 +5,15 @@ from keras.utils import to_categorical
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.image import imread
 
+direktorija = 'training_set/'
+
+# Parādam pirmās 9 bildes. Pārbaudu, vai programma var atrast bildes un direktorija ir korekta.
+for i in range(1, 10):
+    plt.subplot(330 + i)
+    fails = direktorija + 'dog.' + str(i) + '.jpg'
+    bilde = imread(fails)
+    plt.imshow(bilde)
+
+plt.show()
